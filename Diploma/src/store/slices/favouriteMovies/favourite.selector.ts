1c4236e72a2acae271f0cbdbf72d346acf4ef7b9
Vladislav2509 <VladislavDultev@mail.ru>
2022-10-03 11:45:02 +0300

    My Diploma TeachMeSkills

--- Diploma/src/store/slices/favouriteMovies/favourite.selector.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from "../../store";
-
-export const favouriteSelector = (
-  state: RootState
-): RootState["favourite"]["favouriteList"] => state.favourite.favouriteList;
-
-export const favouriteErrorSelector = (
-  state: RootState
-): RootState["favourite"]["error"] => state.favourite.error;
